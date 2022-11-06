30e78051e513896592aaebb2af2c159d6cb1aa9f
meng <mengzhou44@gmail.com>
2022-11-06 16:09:19 -0700

    add an environment environment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Meng Web' });
+  res.render('index', { title: 'Meng Web', env: process.env.NODE_ENV });
 });
 
 module.exports = router;
